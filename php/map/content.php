5cca992a5d13a94a81641b45c4d8f0283b3a1546
yassine <1xfaygo@gmail.com>
2024-10-21 09:08:58 +0200

    21-10-2024//09:08

--- php/map/content.php
+++ php/map/content.php
@@ -1,5 +1,6 @@
 <?php 
 $fruits = array("apple", "pinapple", "pear");
 
-$fruit = implode("     ", $fruits);
-?>
+$fruit = implode("     ", $fruits);?>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab temporibus officiis consequuntur expedita velit! Explicabo deleniti quod, numquam iure modi alias dolorem nisi impedit accusantium quis delectus libero itaque atque?</p>
+<img src="saitama.png">
--- php/map/index.php
+++ php/map/index.php
@@ -1,18 +1,7 @@
 <html>
 <body>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae excepturi id saepe voluptatibus unde numquam laboriosam similique. Quisquam nesciunt beatae fuga corrupti repudiandae dignissimos vero quas itaque laudantium consequatur?</p><br>
-
-<img src=""
-
-
 <?php
-include 'variables.php';
-
-
-echo $fruit
-
-
-
+include 'content.php';
 ?>
 </body>
 </html>
